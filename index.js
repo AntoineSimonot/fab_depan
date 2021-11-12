c20b26c9343cea6e72df882a5e0b512445254d79
Antoine Simonot <simonotantoine1@gmail.com>
2021-11-12 13:28:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//DOM content loaded
+document.addEventListener('DOMContentLoaded', function() {
+   
+    function changeLocation(params) {
+        loc = `https://maps.google.com/maps?width=700&amp&height=440&amp&hl=en&amp&q=${params}%2C%20france+(test)&amp&ie=UTF8&amp&t=&amp&z=10&amp&iwloc=B&amp&output=embed`
+        document.querySelector('iframe').src = loc;
+    }
+
+    document.querySelectorAll(".location").forEach(function(element) {
+        element.addEventListener('click', function() {
+            changeLocation(element.id)
+        })
+    })
+});
